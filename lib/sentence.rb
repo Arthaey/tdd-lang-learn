9e0809f1a84a1fd797d5f7ff765537a2394456bc
Arthaey <arthaey@gmail.com>
2016-07-27 00:33:18 -0700

    Implemented swappable strings.

--- lib/sentence.rb
+++ lib/sentence.rb
@@ -3,13 +3,52 @@ require_relative 'helpers'
 class Sentence < String
   include Helpers
 
-  def initialize(text)
+  attr_reader :parts
+
+  def initialize(text, parts = {})
+    super(text.to_s)
     @original_text = text
-    super(@original_text.to_s)
+
+    @parts = {}
+    parts.each { |k,v| define!(k, v) }
   end
   
   def deformatted
-    chomp_punctuation(uncapitalize(@original_text))
+    new_text = chomp_punctuation(uncapitalize(@original_text))
+    Sentence.new(new_text, @parts)
+  end
+
+  def [](part_name)
+    @parts[part_name]
+  end
+
+  def define!(part_name, match_text)
+    if @original_text !~ /#{Regexp.escape(match_text)}/i
+      raise "'#{match_text}' is not part of '#{@original_text}'"
+    end
+
+    @parts[part_name] = match_text
+  end
+
+  def swap(part_a, part_b)
+    if !@parts.has_key?(part_a)
+      raise "'#{part_a.inspect}' is not a defined part"
+    elsif !@parts.has_key?(part_b)
+      raise "'#{part_b.inspect}' is not a defined part"
+    end
+
+    regex_a = /#{Regexp.escape(self[part_a])}/i
+    regex_b = /#{Regexp.escape(self[part_b])}/i
+
+    match_a = regex_a.match(@original_text)
+    match_b = regex_b.match(@original_text)
+
+    swapped_text = @original_text.sub(regex_a, "~PLACEHOLDER-B~")
+    swapped_text = swapped_text.sub(regex_b, "~PLACEHOLDER-A~")
+    swapped_text = swapped_text.sub("~PLACEHOLDER-B~", match_b[0])
+    swapped_text = swapped_text.sub("~PLACEHOLDER-A~", match_a[0])
+
+    Sentence.new(swapped_text.capitalize, @parts)
   end
 
 end
--- spec/sentence_spec.rb
+++ spec/sentence_spec.rb
@@ -1,13 +1,61 @@
 RSpec.describe Sentence do
 
-  subject { Sentence.new("Hello, world.") }
+  subject(:sentence) { Sentence.new("Hello, world.") }
+
+  subject(:swappable) {
+    Sentence.new("Hello, world.", {greeting: "hello", addressee: "world"})
+  }
 
   it "should save the original text" do
-    expect(subject.to_s).to eq("Hello, world.")
+    expect(sentence.to_s).to eq("Hello, world.")
   end
 
   it "#deformatted should remove formatting" do
-    expect(subject.deformatted).to eq("hello, world")
+    expect(sentence.deformatted).to eq("hello, world")
+  end
+
+  context "with defined parts" do
+    it "should create parts via the initializer" do
+      expect(swappable[:greeting]).to eq("hello")
+    end
+
+    it "should create parts after initialization" do
+      sentence.define!(:greeting, "hello")
+      expect(sentence[:greeting]).to eq("hello")
+    end
+
+    it "should fail to create parts that don't exist in the text" do
+      expect {
+        swappable.define!(:non_matching_part, "404")
+      }.to raise_error("'404' is not part of 'Hello, world.'")
+    end
+
+    it "#swap should exchange parts of the text" do
+      expect(swappable.swap(:greeting, :addressee)).to eq("World, hello.")
+      expect(swappable.swap(:addressee, :greeting)).to eq("World, hello.")
+      expect(swappable.to_s).to eq("Hello, world.")
+    end
+
+    it "#swap should fail with undefined parts" do
+      expect {
+        swappable.swap(:non_existent_part, :other_part)
+      }.to raise_error("':non_existent_part' is not a defined part")
+    end
+  end
+
+  context "allows chaining methods" do
+    it "with #deformatted" do
+      expect(sentence.deformatted).to be_a(Sentence)
+    end
+
+    it "with #swap" do
+      expect(swappable.swap(:greeting, :addressee)).to be_a(Sentence)
+    end
+
+    it "with duplicated parts" do
+      new_swappable = swappable.swap(:greeting, :addressee)
+      expect(swappable.parts).to eq(new_swappable.parts)
+    end
   end
 
 end
